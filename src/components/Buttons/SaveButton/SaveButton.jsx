1d4a95b128293357af308488d456502086dbe2bc
maximsan <maximuzz@yandex.ru>
2021-02-27 22:11:53 +0300

    fix: fix buttons tests

--- src/components/Buttons/SaveButton/SaveButton.jsx
+++ src/components/Buttons/SaveButton/SaveButton.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 import { Button } from '../Button/Button';
 
-export const SaveButton = ({ children }) => <Button>{children}</Button>;
+export const SaveButton = ({ children, ...restProps }) => <Button {...restProps}>{children}</Button>;
 
 SaveButton.propTypes = {
   children: PropTypes.node,
+  name: PropTypes.string.isRequired,
+  styles: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
 };
 SaveButton.defaultProps = {
   children: 'Save',
